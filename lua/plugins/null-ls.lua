60c24a4621b43e85e1d1fceaf6be466a17512bbe
Lennart Koziollek <lennart.koziollek@check24.de>
2023-09-25 08:52:36 +0200

    misc

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,16 +1,16 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  dependendcies = { 'VonHeikemen/lsp-zero.nvim' },
-  event = { "BufReadPre", "BufNewFile" },
-  opts = function()
-    local nls = require("null-ls")
-    return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-      sources = {
-        nls.builtins.formatting.stylua,
-        nls.builtins.formatting.alejandra,
-        nls.builtins.formatting.gofmt,
-      },
-    }
-  end,
+	"jose-elias-alvarez/null-ls.nvim",
+	dependendcies = { "VonHeikemen/lsp-zero.nvim" },
+	event = { "BufReadPre", "BufNewFile" },
+	opts = function()
+		local nls = require("null-ls")
+		return {
+			root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+			sources = {
+				nls.builtins.formatting.stylua,
+				nls.builtins.formatting.alejandra,
+				nls.builtins.formatting.gofmt,
+			},
+		}
+	end,
 }
