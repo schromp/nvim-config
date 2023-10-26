0a8479a98711e9283124da085e2af17377891741
Lennart Koziollek <lennart.koziollek@check24.de>
2023-10-26 18:59:39 +0200

    null-ls fix

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -10,7 +10,9 @@ return {
       nls.builtins.formatting.stylua,
       nls.builtins.formatting.alejandra,
       nls.builtins.formatting.gofmt,
+      nls.builtins.formatting.yq,
       nls.builtins.diagnostics.saltlint,
+      nls.builtins.diagnostics.yamllint,
     }
     nls.setup({sources = sources})
   end,
