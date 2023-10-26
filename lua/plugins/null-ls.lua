e43263724c4252653f4d96f6586c815a06274742
Lennart Koziollek <lennart.koziollek@check24.de>
2023-10-26 09:40:31 +0200

    Temporary change to null-ls for salt-lint aswell as fixed null-ls (it never worked before)

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,17 +1,17 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	enabled = false,
-	dependendcies = { "VonHeikemen/lsp-zero.nvim" },
-	event = { "BufReadPre", "BufNewFile" },
-	opts = function()
-		local nls = require("null-ls")
-		return {
-			root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-			sources = {
-				nls.builtins.formatting.stylua,
-				nls.builtins.formatting.alejandra,
-				nls.builtins.formatting.gofmt,
-			},
-		}
-	end,
+  "jose-elias-alvarez/null-ls.nvim",
+  enabled = true,
+  dependendcies = { "VonHeikemen/lsp-zero.nvim" },
+  -- event = { "BufReadPre", "BufNewFile" },
+  opts = function()
+    local nls = require("null-ls")
+    local root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+    local sources = {
+      nls.builtins.formatting.stylua,
+      nls.builtins.formatting.alejandra,
+      nls.builtins.formatting.gofmt,
+      nls.builtins.diagnostics.saltlint,
+    }
+    nls.setup({sources = sources})
+  end,
 }
