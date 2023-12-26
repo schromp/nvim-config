91e60d9d69c76186a5a6daa0271170c9a3380a77
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2023-12-26 12:06:21 +0100

    added ts tools and clangd extensions

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -1,78 +1,93 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v2.x",
-	enabled = true,
-	dependencies = {
-		-- LSP Support
-		{ "neovim/nvim-lspconfig" }, -- Required
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		enabled = true,
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
 
-		-- Autocompletion
-		{ "hrsh7th/nvim-cmp" }, -- Required
-		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-		{ "hrsh7th/cmp-path" }, -- Required
-		{ "hrsh7th/cmp-buffer" },
-		{ "saadparwaiz1/cmp_luasnip" }, -- Required
-		{ "elkowar/yuck.vim" },
-		{ "vmware-archive/salt-vim" },
-		{ "folke/neodev.nvim" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-path" }, -- Required
+			{ "hrsh7th/cmp-buffer" },
+			{ "saadparwaiz1/cmp_luasnip" }, -- Required
+			{ "elkowar/yuck.vim" },
+			{ "vmware-archive/salt-vim" },
+			{ "folke/neodev.nvim" },
+		},
+		config = function()
+			local lsp = require("lsp-zero").preset({})
+
+			lsp.on_attach(function(client, bufnr)
+				lsp.default_keymaps({ buffer = bufnr })
+				local opts = { buffer = bufnr }
+
+				vim.keymap.set({ "n", "x" }, "gq", function()
+					vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+				end, opts)
+				vim.keymap.set("n", "<space>la", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<space>lr", vim.lsp.buf.rename, opts)
+
+				require("clangd_extensions.inlay_hints").setup_autocmd()
+				require("clangd_extensions.inlay_hints").set_inlay_hints()
+			end)
+
+			-- When you don't have mason.nvim installed
+			-- You'll need to list the servers installed in your system
+			lsp.setup_servers({ "nil_ls", "pyright", "lua_ls", "phpactor", "gopls", "tsserver", "cssls", "clangd" })
+
+			-- (Optional) Configure lua language server for neovim
+			require("lspconfig").texlab.setup({
+				build = {
+					args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+					executable = "latexmk",
+					forwardSearchAfter = true,
+					onSave = true,
+				},
+			})
+
+			lsp.setup()
+
+			local cmp = require("cmp")
+			local cmp_action = require("lsp-zero").cmp_action()
+
+			---require('luasnip.loaders.from_vscode').lazy_load()
+
+			cmp.setup({
+				sources = {
+					{ name = "path" },
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "luasnip", keyword_length = 2 },
+					{ name = "cody" },
+				},
+				mapping = {
+					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				},
+				window = {
+					completeopt = "menu,menuone,noinsert",
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+			})
+
+			vim.api.nvim_set_hl(0, "CmpItemKindCody", { fg = "Red" })
+		end,
 	},
-	config = function()
-		local lsp = require("lsp-zero").preset({})
-
-		lsp.on_attach(function(client, bufnr)
-			lsp.default_keymaps({ buffer = bufnr })
-			local opts = { buffer = bufnr }
-
-			vim.keymap.set({ "n", "x" }, "gq", function()
-				vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
-			end, opts)
-			vim.keymap.set("n", "<space>la", vim.lsp.buf.code_action, opts)
-			vim.keymap.set("n", "<space>lr", vim.lsp.buf.rename, opts)
-		end)
-
-		-- When you don't have mason.nvim installed
-		-- You'll need to list the servers installed in your system
-		lsp.setup_servers({ "nil_ls", "pyright", "lua_ls", "phpactor", "gopls", "tsserver", "cssls", "clangd"})
-
-		-- (Optional) Configure lua language server for neovim
-		require("lspconfig").texlab.setup({
-			build = {
-				args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-				executable = "latexmk",
-				forwardSearchAfter = true,
-				onSave = true,
-			},
-		})
-
-		lsp.setup()
-
-		local cmp = require("cmp")
-		local cmp_action = require("lsp-zero").cmp_action()
-
-		---require('luasnip.loaders.from_vscode').lazy_load()
-
-		cmp.setup({
-			sources = {
-				{ name = "path" },
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "buffer", keyword_length = 3 },
-				{ name = "luasnip", keyword_length = 2 },
-				{ name = "cody" },
-			},
-			mapping = {
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			},
-			window = {
-				completeopt = "menu,menuone,noinsert",
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-		})
-
-    vim.api.nvim_set_hl(0, "CmpItemKindCody", { fg = "Red" })
-
-	end,
+	{
+		"p00f/clangd_extensions.nvim",
+		dependencies = {
+			"VonHeikemen/lsp-zero.nvim",
+		},
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
 }
