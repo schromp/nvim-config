6659cb720adc63aa659949dadf1ac8d4c13dade7
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-01-04 21:27:43 +0100

    switch from null-ls to conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+  keys = {
+    { "gq",
+      function ()
+        require("conform").format({})
+      end,
+      desc = "Format the file"},
+  },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+      go = { "gofmt" },
+      nix = { "alejandra" },
+      sls = { "saltlint" },
+      yaml = { { "prettierd", "prettier" } },
+			javascript = { { "prettierd", "prettier" } },
+		},
+	},
+}
