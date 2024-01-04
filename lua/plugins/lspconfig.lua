7d338990a445735cc1188705bf4ffbb075bc8434
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-01-04 21:26:45 +0100

    switched from lsp-zero to plain lspconfig

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,101 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "folke/neodev.nvim" },
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-path" }, -- Required
+			{ "hrsh7th/cmp-buffer" },
+			{ "L3MON4D3/LuaSnip" },
+			{ "saadparwaiz1/cmp_luasnip" }, -- Required
+			{ "vmware-archive/salt-vim" },
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			lspconfig.gopls.setup({})
+			lspconfig.nil_ls.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.pyright.setup({})
+			lspconfig.phpactor.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.emmet_ls.setup({})
+			lspconfig.texlab.setup({
+				build = {
+					args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+					executable = "latexmk",
+					forwardSearchAfter = true,
+					onSave = true,
+				},
+			})
+
+			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+
+				callback = function(ev)
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+					-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+					-- vim.keymap.set('n', '<space>wl', function()
+					--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					-- end, opts)
+					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<space>lr", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<space>la", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				end,
+			})
+
+			local cmp = require("cmp")
+
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				sources = {
+					{ name = "path" },
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "luasnip", keyword_length = 2 },
+					{ name = "cody" },
+				},
+				mapping = {
+					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				},
+				window = {
+					completeopt = "menu,menuone,noinsert",
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+			})
+
+			vim.api.nvim_set_hl(0, "CmpItemKindCody", { fg = "Red" })
+		end,
+	},
+	{
+		"p00f/clangd_extensions.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
+	},
+}
