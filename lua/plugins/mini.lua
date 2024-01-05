28d1021c8f8186275b72814fc8d910426d0109c6
Lennart Koziollek <lennart.koziollek@check24.de>
2024-01-05 10:17:28 +0100

    changed oil to mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,23 +1,51 @@
 return {
 	"echasnovski/mini.nvim",
 	version = false,
-	config = function()
-		-- require("mini.comment").setup({
-		-- 	options = {
-		-- 		custom_commentstring = function()
-		-- 			return vim.bo.commentstring
-		-- 		end,
-		-- 	},
-		-- })
-		-- require('mini.completion').setup()
+	keys = {
+		-- { "<leader>fe", "<cmd>lua  },
+		{
+			"<leader>fe",
+			function()
+				require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+			end,
+			desc = "Open mini.files (directory of current file)",
+		},
+		{
+			"<leader>fE",
+			function()
+				require("mini.files").open(vim.loop.cwd(), true)
+			end,
+			desc = "Open mini.files (cwd)",
+		},
+	},
+	opts = {},
+	config = function(_, opts)
 		require("mini.pairs").setup()
 		require("mini.surround").setup()
-    -- require("mini.indentscope").setup({
-    --   draw = {
-    --     delay = 0,
-    --     animation = require("mini.indentscope").gen_animation.none(),
-    --   },
-    --   symbol = "|"
-    -- })
+
+		-- files
+		require("mini.files").setup(opts)
+		local show_dotfiles = true
+		local filter_show = function(fs_entry)
+			return true
+		end
+		local filter_hide = function(fs_entry)
+			return not vim.startswith(fs_entry.name, ".")
+		end
+
+		local toggle_dotfiles = function()
+			show_dotfiles = not show_dotfiles
+			local new_filter = show_dotfiles and filter_show or filter_hide
+			require("mini.files").refresh({ content = { filter = new_filter } })
+		end
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniFilesBufferCreate",
+			callback = function(args)
+				local buf_id = args.data.buf_id
+				-- Tweak left-hand side of mapping to your liking
+				vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+			end,
+		})
 	end,
 }
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,5 +1,6 @@
 return {
 	"stevearc/oil.nvim",
+  enabled = false,
 	keys = {
 		{ "<leader>fe", "<CMD>Oil<CR>", desc = "Open Oil" },
 	},
