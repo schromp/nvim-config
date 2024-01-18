b3759b330bb19ac072af79a2cc76a9a1ac9823a3
Lennart Koziollek <lennart.koziollek@check24.de>
2024-01-18 14:43:14 +0100

    format the format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,19 +1,21 @@
 return {
 	"stevearc/conform.nvim",
-  keys = {
-    { "gq",
-      function ()
-        require("conform").format({})
-      end,
-      desc = "Format the file"},
-  },
+	keys = {
+		{
+			"gq",
+			function()
+				require("conform").format({})
+			end,
+			desc = "Format the file",
+		},
+	},
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-      go = { "gofmt" },
-      nix = { "alejandra" },
-      sls = { "saltlint" },
-      yaml = { { "prettierd", "prettier" } },
+			go = { "gofmt" },
+			nix = { "alejandra" },
+			sls = { "saltlint" },
+			yaml = { { "prettierd", "prettier" } },
 			javascript = { { "prettierd", "prettier" } },
 		},
 	},
