c517d1ac5e699bb597b32726c0666289df64344f
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-03-03 22:30:58 +0100

    added toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	keys = {
+		{ "<C-]>", "<cmd>ToggleTerm<cr>", desc = "Open a terminal" },
+	},
+	config = function()
+		require("toggleterm").setup({})
+
+		function _G.set_terminal_keymaps()
+			local opts = { buffer = 0 }
+			vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+			vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+			vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+			vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+			vim.keymap.set("t", "<C-]>", [[<Cmd>ToggleTerm<CR>]], opts)
+		end
+
+		vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+	end,
+}
