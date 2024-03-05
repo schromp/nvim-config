5422a0119e1dacfa46a3f329a1f9aa68576e6b9d
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-03-05 19:57:22 +0100

    moved json to dynconfig

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-JsonVariables = require "lib.json"
+JsonVariables = require "lib.dynconfig"
 
 require "core.options"
 require "core.keymaps"
