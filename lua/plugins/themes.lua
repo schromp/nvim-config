2ce2502428faee13b49e79adf4f5d3b5a53ab1b8
Lennart Koziollek <lennart.koziollek@check24.de>
2024-04-29 13:56:03 +0200

    cyberdream theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -93,4 +93,19 @@ return {
 		priority = 1000,
 		opts = {},
 	},
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				-- Recommended - see "Configuring" below for more config options
+				transparent = false,
+				italic_comments = true,
+				hide_fillchars = true,
+				borderless_telescope = true,
+				terminal_colors = true,
+			})
+		end,
+	},
 }
