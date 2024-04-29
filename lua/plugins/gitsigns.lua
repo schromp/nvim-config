10df681ab34f4af37c3b5ca0906cc1e8c02693f9
Lennart Koziollek <lennart.koziollek@check24.de>
2024-04-29 13:55:55 +0200

    gitsignes toggle deleted keybind

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,8 @@
 return {
   "lewis6991/gitsigns.nvim",
   keys = {
-    { "<leader>gs", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle Telescope File Tree" },
+    { "<leader>gs", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle current line blame" },
+    { "<leader>gd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "Toggle showing delete lines" },
   },
   lazy = false,
   config = function()
