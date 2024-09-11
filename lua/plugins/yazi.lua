c06c1ae5e2bf9736f56b76f5466c15ecf28782ec
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-09-11 16:24:08 +0200

    yazi plugin

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,15 @@
+return {
+	"mikavilpas/yazi.nvim",
+	keys = {
+		{
+			"<leader>fe",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+		{
+			"<leader>fn",
+			"<cmd>Yazi<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+}
