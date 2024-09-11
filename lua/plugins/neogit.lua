d75f7c4f9563daac56a81613398feeee6ec9fac5
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-09-11 16:23:19 +0200

    diffview keybinds

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,19 +1,26 @@
 return {
-  "TimUntersberger/neogit",
-  dependencies = {
-    "sindrets/diffview.nvim",
-  },
-  keys = {
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Toggle Neogit" }
-  },
-  enabled = true,
-  config = function()
-    local neogit = require("neogit")
+	"TimUntersberger/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim",
 
-    neogit.setup {
-      integrations = {
-        diffview = true
-      }
-    }
-  end,
+		"nvim-telescope/telescope.nvim", -- optional
+	},
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Toggle Neogit" },
+    { "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview"},
+    { "<leader>dq", "<cmd>DiffviewClose<cr>", desc = "Close Diffview"},
+    { "<leader>df", "<cmd>DiffviewFileHistory %<cr>", desc = "Show file history"},
+	},
+	enabled = true,
+	config = function()
+		local neogit = require("neogit")
+
+		neogit.setup({
+			integrations = {
+				diffview = true,
+			},
+			graph_style = "unicode",
+		})
+	end,
 }
