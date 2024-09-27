b7872e8538d111fed0930489fe915682a0254d9d
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-09-27 15:10:37 +0200

    neovide blur

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -1,6 +1,7 @@
 if vim.g.neovide then
-	vim.g.neovide_transparency = 1
+	vim.g.neovide_transparency = 0.8
 	vim.g.neovide_scroll_animation_length = 0.1
 	vim.g.neovide_cursor_trail_size = 0
 	vim.g.neovide_fullscreen = false
+	vim.g.neovide_window_blurred = true
 end
