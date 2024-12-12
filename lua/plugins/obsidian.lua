625feccff3dec1af30854c1c8744a3111dcb167d
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-12-12 08:29:11 +0100

    obisidian path changes

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,16 +2,16 @@ return {
 	"epwalsh/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
-	-- ft = "markdown",
+	ft = "markdown",
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	event = {
-		-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-		"BufReadPre "
-			.. vim.fn.expand("~")
-			.. "/OneDrive/repos/vaults/*/**.md",
-		"BufNewFile " .. vim.fn.expand("~") .. "/OneDrive/repos/vaults/*/**.md",
-	},
+	-- event = {
+	-- 	-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	-- 	-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+	-- 	"BufReadPre "
+	-- 		.. vim.fn.expand("~")
+	-- 		.. "/OneDrive/repos/vaults/*/**.md",
+	-- 	"BufNewFile " .. vim.fn.expand("~") .. "/OneDrive/repos/vaults/*/**.md",
+	-- },
 	dependencies = {
 		-- Required.
 		"nvim-lua/plenary.nvim",
@@ -21,9 +21,13 @@ return {
 	},
 	opts = {
 		workspaces = {
+			-- {
+			-- 	name = "personal",
+			-- 	path = "~/repos/vaults/obsidian-lk",
+			-- },
 			{
-				name = "personal",
-				path = "~/repos/vaults/obsidian-lk",
+				name = "indi-admin-documentation",
+				path = "~/Repos/indi-admin-documentation",
 			},
 		},
 
