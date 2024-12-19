f95eb4cd5bd6389cebbdcf25015e9ab037e289f9
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-12-19 09:18:30 +0100

    fzf-lua: changed from telescope to fzf-lua

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,53 @@
+return {
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"<leader>fr",
+			function()
+				require("fzf-lua").resume()
+			end,
+			desc = "Resume last query",
+		},
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fa",
+			function()
+				require("fzf-lua").grep()
+			end,
+			desc = "Grep all files",
+		},
+		{
+			"<leader>fs",
+			function()
+				require("fzf-lua").grep_curbuf()
+			end,
+			desc = "Grep current file",
+		},
+		{
+			"<leader>fu",
+			function()
+				require("fzf-lua").grep_cWORD()
+			end,
+			desc = "Grep word under cursor",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Find buffers",
+		},
+	},
+	config = function()
+		-- calling `setup` is optional for customization
+		require("fzf-lua").setup({})
+	end,
+}
