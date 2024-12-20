96a902d210991beef71d23a20bc3d9776fa66170
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-12-20 16:09:27 +0100

    snacks: terminal ctrl switch

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,6 +13,16 @@ return {
 				wo = { wrap = true }, -- Wrap notifications
 			},
 		},
+		-- terminal = {
+		-- 	win = {
+		-- 		keys = {
+		-- 			nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+		-- 			nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+		-- 			nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+		-- 			nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+		-- 		},
+		-- 	},
+		-- },
 	},
 	keys = {
 		{
