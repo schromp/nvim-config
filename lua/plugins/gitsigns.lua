c98af5b577318a91a4fbc370c2681d0a6579e03a
Lennart Koziollek <lennart.koziollek@check24.de>
2025-04-23 11:52:45 +0200

    gitsigns: keymap to copy current branch name

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,6 +3,7 @@ return {
   keys = {
     { "<leader>gs", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle current line blame" },
     { "<leader>gd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "Toggle showing delete lines" },
+    { "<leader>gc", ":let @+ = system('git branch --show-current')<CR>", desc = "Copy the current branch name" },
   },
   lazy = false,
   config = function()
