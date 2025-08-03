dec6612b0e8f7e9b08c949576c3e85e7199a6a06
schromp <lennart@koziollek.com>
2025-08-03 23:02:47 +0200

    persistant-terminals: mini progress

--- lua/persistant-terminals/init.lua
+++ lua/persistant-terminals/init.lua
@@ -5,13 +5,18 @@ M.setup = function()
 end
 
 M.newTerm = function(name)
-  vim.fn.jobstart({ "shpool", "attach", name }, { term = true })
+	vim.fn.jobstart({ "shpool", "attach", name }, { term = true })
 end
 
-M.listTerms = function ()
-  -- TODO: dont use awk and parse the output of shpool list directly
-  require'fzf-lua'.fzf_exec("shpool list | awk 'NR>1 {print $1}'")
+M.findTerms = function()
+	-- TODO: dont use awk and parse the output of shpool list directly
+	require("fzf-lua").fzf_exec("shpool list | awk 'NR>1 {print $1}'", {
+		actions = {
+			["default"] = function(selected)
+				M.newTerm(selected[1])
+			end,
+		},
+	})
 end
 
 return M
-
