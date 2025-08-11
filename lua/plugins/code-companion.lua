64bc582ed081d69fe1be07765e6d5f184c71f3e0
Lennart Koziollek <lennart.koziollek@check24.de>
2025-08-11 09:15:30 +0200

    mcphub code companion

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -20,14 +20,21 @@ return {
 			},
 		},
 		extensions = {
-			-- mcphub = {
-			-- 	callback = "mcphub.extensions.codecompanion",
-			-- 	opts = {
-			-- 		make_vars = true,
-			-- 		make_slash_commands = true,
-			-- 		show_result_in_chat = true,
-			-- 	},
-			-- },
+			mcphub = {
+				callback = "mcphub.extensions.codecompanion",
+				opts = {
+					-- MCP Tools
+					make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+					show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
+					add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+					show_result_in_chat = true, -- Show tool results directly in chat buffer
+					format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+					-- MCP Resources
+					make_vars = true, -- Convert MCP resources to #variables for prompts
+					-- MCP Prompts
+					make_slash_commands = true, -- Add MCP prompts as /slash commands
+				},
+			},
 		},
 	},
 	dependencies = {
@@ -50,13 +57,25 @@ return {
 						},
 					},
 					filetypes = {
-            yaml = true,
+						yaml = true,
 					},
 				})
 			end,
 		},
 		"nvim-treesitter/nvim-treesitter",
-		-- "ravitemer/mcphub.nvim",
+		{
+			"ravitemer/mcphub.nvim",
+			ft = { "codecompanion" },
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+			build = "bundled_build.lua",
+			config = function()
+				require("mcphub").setup({
+					use_bundled_binary = true,
+				})
+			end,
+		},
 		{
 			"MeanderingProgrammer/render-markdown.nvim",
 			ft = { "markdown", "codecompanion" },
