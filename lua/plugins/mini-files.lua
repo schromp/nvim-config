339f877621218cef4acd059ba239300bd48044de
Lennart Koziollek <lennart.koziollek@check24.de>
2026-03-13 13:45:15 +0100

    cleanup mini files

--- lua/plugins/mini-files.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({"https://github.com/nvim-mini/mini.files"})
-
-require("mini.files").setup({})
-
-vim.keymap.set("n", "<leader>e", require("mini.files").open, { desc = "Open File Picker" })
