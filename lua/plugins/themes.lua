ceb3756096bdadc97d821c4cbda0c16ed53925eb
schromp <schromp@echsen.club>
2026-06-11 16:11:20 +0200

    cyberdream theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,19 +1,20 @@
 vim.pack.add({
-  "https://github.com/rose-pine/neovim",
-  "https://github.com/bluz71/vim-moonfly-colors",
-  "https://github.com/kdheepak/monochrome.nvim",
-  "https://github.com/catppuccin/nvim"
+	"https://github.com/rose-pine/neovim",
+	"https://github.com/bluz71/vim-moonfly-colors",
+	"https://github.com/kdheepak/monochrome.nvim",
+	"https://github.com/catppuccin/nvim",
+	"https://github.com/scottmckendry/cyberdream.nvim",
 })
 
 require("rose-pine").setup({
-  styles = {
-    transparency = true,
-  }
+	styles = {
+		transparency = true,
+	},
 })
 
 require("catppuccin").setup({
-  flavour = "mocha",
-  transparent_background = false,
+	flavour = "mocha",
+	transparent_background = false,
 })
 
 vim.cmd("colorscheme catppuccin-nvim")
